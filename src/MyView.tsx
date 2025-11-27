b52c5da763907c0e8cda80dca2b5d40af6c3b0f7
simon <simonyen0528@gmail.com>
2025-11-27 14:45:51 +0800

    MYVIEW

---
+++ src/MyView.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+export const MyView: React.FC = () => {
+
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50 transition-opacity duration-300">
+        HI
+    </div>
+  );
+};
